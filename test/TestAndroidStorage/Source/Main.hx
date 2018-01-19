e0ef996de1b3c46c45ddba84440a0c40d6236083
Jonas Malaco <jonas@protocubo.io>
2018-01-19 18:35:17 -0200

    Test file system operations within the sample project

--- test/TestAndroidStorage/Source/Main.hx
+++ test/TestAndroidStorage/Source/Main.hx
@@ -1,7 +1,10 @@
 package;
 
 import Assertion.*;
+import haxe.io.Path;
 import openfl.display.Sprite;
+import sys.FileSystem;
+import sys.io.File;
 
 class Main extends Sprite {
 	public function new () {
@@ -11,6 +14,7 @@ class Main extends Sprite {
 #if android
 		var buf = [];
 		var ext = extension.AndroidStorage.getExternalStorage();
+		show(ext.length);
 		for (i in ext) {
 			var available = Math.floor(i.availableBytes/1024/1024);
 			var total = Math.round(i.totalBytes/1024/1024);
@@ -18,8 +22,22 @@ class Main extends Sprite {
 			var msg = '${i.path} (emulated=${i.emulated}, removable=${i.removable}) has $available MiB available out of a total size of $total MiB';
 			show(msg);
 			buf.push(msg);
+
+			try {
+				var tpath = Path.join([i.path, ".openfl-android-storage"]);
+				var now = Date.now();
+
+				File.saveContent(tpath, now.toString());
+				var read = File.getContent(tpath);
+				show(tpath, now.toString(), read);
+				assert(now.toString() == read);
+
+				FileSystem.rename(tpath, tpath + ".1");
+				show(FileSystem.stat(tpath + ".1"));
+			} catch (err:Dynamic) {
+				show("FAILED", err);
+			}
 		}
-		show(buf.length);
 		// var t = new openfl.text.TextField();
 		// t.width = 800;
 		// t.text = buf.join("\r");
