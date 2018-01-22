1b7c66bca826c4053230b9993c0b7b3780172dd8
Jonas Malaco <jonas@protocubo.io>
2018-01-22 18:29:26 -0200

    Clean the output and clarifiy the result of running the sample project

--- test/TestAndroidStorage/Source/Main.hx
+++ test/TestAndroidStorage/Source/Main.hx
@@ -12,36 +12,39 @@ class Main extends Sprite {
 		super ();
 		
 #if android
-		var buf = [];
-		var ext = extension.AndroidStorage.getExternalStorage();
-		show(ext.length);
-		for (i in ext) {
-			var available = Math.floor(i.availableBytes/1024/1024);
-			var total = Math.round(i.totalBytes/1024/1024);
-			show(i.path, i.emulated, i.removable, i.probablyRemovable, available, total);
-			var msg = '${i.path} (emulated=${i.emulated}, removable=${i.removable}) has $available MiB available out of a total size of $total MiB';
-			show(msg);
-			buf.push(msg);
+		var errcnt = 0;
+		try {
+			var ext = extension.AndroidStorage.getExternalStorage();
+			show(ext.length);
+			for (i in ext) {
+				var available = Math.floor(i.availableBytes/1024/1024);
+				var total = Math.round(i.totalBytes/1024/1024);
+				trace('${i.path} (emulated=${i.emulated}, removable=${i.removable}) has $available MiB available out of a total size of $total MiB');
+				show(i);
 
-			try {
-				var tpath = Path.join([i.path, ".openfl-android-storage"]);
-				var now = Date.now();
+				try {
+					var tpath = Path.join([i.path, ".openfl-android-storage"]);
+					var now = Date.now();
 
-				File.saveContent(tpath, now.toString());
-				var read = File.getContent(tpath);
-				show(tpath, now.toString(), read);
-				assert(now.toString() == read);
+					File.saveContent(tpath, now.toString());
+					var read = File.getContent(tpath);
+					show(tpath, now.toString(), read);
+					assert(now.toString() == read);
 
-				FileSystem.rename(tpath, tpath + ".1");
-				show(FileSystem.stat(tpath + ".1"));
-			} catch (err:Dynamic) {
-				show("FAILED", err);
+					FileSystem.rename(tpath, tpath + ".1");
+					show(FileSystem.stat(tpath + ".1"));
+				} catch (e2:Dynamic) {
+					trace("FAILED");
+					show(e2);
+					errcnt++;
+				}
 			}
+		} catch (e1:Dynamic) {
+			trace("FAILED");
+			show(e1);
+			errcnt++;
 		}
-		// var t = new openfl.text.TextField();
-		// t.width = 800;
-		// t.text = buf.join("\r");
-		// addChild(t);
+		trace("RESULT: " + (errcnt == 0 ? "all good" : "ERROR"));
 #end
 	}
 }
