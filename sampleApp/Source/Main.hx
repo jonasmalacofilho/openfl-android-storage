86cbd746da4e00df8bf096fe2eb7443920cf582c
Jonas Malaco <jonas@protocubo.io>
2018-01-22 18:41:40 -0200

    Add comments to the example project

--- sampleApp/Source/Main.hx
+++ sampleApp/Source/Main.hx
@@ -7,45 +7,61 @@ import sys.FileSystem;
 import sys.io.File;
 
 class Main extends Sprite {
+	var errCnt:Int;
+
 	public function new () {
-		
 		super ();
-		
 #if android
-		var errcnt = 0;
+		trace("STARTING NEW EXECUTION AT " + Date.now());
+		errCnt = 0;
 		try {
-			var ext = extension.AndroidStorage.getExternalStorage();
-			show(ext.length);
-			for (i in ext) {
-				var available = Math.floor(i.availableBytes/1024/1024);
-				var total = Math.round(i.totalBytes/1024/1024);
-				trace('${i.path} (emulated=${i.emulated}, removable=${i.removable}) has $available MiB available out of a total size of $total MiB');
-				show(i);
-
-				try {
-					var tpath = Path.join([i.path, ".openfl-android-storage"]);
-					var now = Date.now();
-
-					File.saveContent(tpath, now.toString());
-					var read = File.getContent(tpath);
-					show(tpath, now.toString(), read);
-					assert(now.toString() == read);
-
-					FileSystem.rename(tpath, tpath + ".1");
-					show(FileSystem.stat(tpath + ".1"));
-				} catch (e2:Dynamic) {
-					trace("FAILED");
-					show(e2);
-					errcnt++;
-				}
-			}
+			runExternalStorageExample();
 		} catch (e1:Dynamic) {
 			trace("FAILED");
 			show(e1);
-			errcnt++;
+			errCnt++;
 		}
-		trace("RESULT: " + (errcnt == 0 ? "all good" : "ERROR"));
+		trace("EXECUTION RESULT: " + (errCnt == 0 ? "all good" : "ERROR"));
+#else
+		trace("Nothing to show for !android builds");
 #end
+	}
+
+	function runExternalStorageExample()
+	{
+		/* get the available paths for external storage */
+		var ext = extension.AndroidStorage.getExternalStorage();
+		show(ext.length);
+
+		for (i in ext) {
+			show(i);
+
+			/* print basic information about this path */
+			var available = Math.floor(i.availableBytes/1024/1024);
+			var total = Math.round(i.totalBytes/1024/1024);
+			trace('${i.path} (emulated=${i.emulated}, removable=${i.removable}) has $available MiB available out of a total size of $total MiB');
+
+			try {
+				var tpath = Path.join([i.path, ".openfl-android-storage"]);
+				var now = Date.now();
+
+				/* save a timestamp in a temporary file */
+				File.saveContent(tpath, now.toString());
+
+				/* read it back and check if it matches what we wrote */
+				var read = File.getContent(tpath);
+				show(tpath, now.toString(), read);
+				assert(now.toString() == read);
+
+				/* use other fs operations like rename and stat */
+				FileSystem.rename(tpath, tpath + ".1");
+				show(FileSystem.stat(tpath + ".1"));
+			} catch (err:Dynamic) {
+				trace("FAILED");
+				show(err);
+				errCnt++;
+			}
+		}
 	}
 }
 
